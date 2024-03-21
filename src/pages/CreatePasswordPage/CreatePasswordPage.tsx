0f789fdb204c63dcab283b993a5e3e2bb44ac9de
Ruslan <Giovingo92@gmail.com>
2024-03-21 15:28:25 +0200

    feat: media screen icon show password

--- src/pages/CreatePasswordPage/CreatePasswordPage.tsx
+++ src/pages/CreatePasswordPage/CreatePasswordPage.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import { useAuth } from "../../hooks/useAuth";
-import { Button, Input, Logo, Title } from "../../components";
+import { Button, Icon, Input, Logo, Title } from "../../components";
 
 import styles from "../SignUpPage/SignUpPage.module.css";
 
 const CreatePasswordPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirm, setShowConfirm] = useState(false);
+
   const { navigation } = useAuth();
+
+  const toggle = () => setShowPassword((prevState) => !prevState);
+  const toggleConfirm = () => setShowConfirm((prevState) => !prevState);
 
   return (
     <section>
@@ -16,11 +23,31 @@ const CreatePasswordPage = () => {
       </div>
       <div className={styles.wrapper__password}>
         <p className={styles.password}>Password</p>
-        <Input placeholder="Password" />
+        <div className={styles.box}>
+          <Input
+            placeholder="Password"
+            type={showPassword ? "text" : "password"}
+          />
+          {showPassword ? (
+            <Icon idIcon="hide" onClick={toggle} />
+          ) : (
+            <Icon idIcon="show" onClick={toggle} />
+          )}
+        </div>
       </div>
       <div className={styles.wrapper__password}>
         <p className={styles.password}>Confirm Password</p>
-        <Input placeholder="Password" />
+        <div className={styles.box}>
+          <Input
+            placeholder="Password"
+            type={showConfirm ? "text" : "password"}
+          />
+          {showConfirm ? (
+            <Icon idIcon="hide" onClick={toggleConfirm} />
+          ) : (
+            <Icon idIcon="show" onClick={toggleConfirm} />
+          )}
+        </div>
       </div>
       <div className={styles.wrapper__button}>
         <Button onClick={() => navigation("signin")}>Reset Password</Button>
